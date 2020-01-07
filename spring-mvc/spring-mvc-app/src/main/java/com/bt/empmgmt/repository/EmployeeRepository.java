3f92efa441e6f5bb53997c667b35b68d4f099b8e
prashdeep <pradeep.kumar44@gmail.com>
2020-01-07 11:29:08 +0530

    committing the changes

---
+++ spring-mvc/spring-mvc-app/src/main/java/com/bt/empmgmt/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.bt.empmgmt.repository;
+
+import com.bt.empmgmt.model.Employee;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long > {
+
+    @Query("select emp_id, emp_name from employe where emp_id = :name")
+    public Employee listByName( String name);
+}
--- spring-mvc/spring-mvc-app/src/main/java/com/bt/empmgmt/service/EmployeeServiceImpl.java
+++ spring-mvc/spring-mvc-app/src/main/java/com/bt/empmgmt/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.bt.empmgmt.service;
 
 import com.bt.empmgmt.dao.EmployeeDAO;
 import com.bt.empmgmt.model.Employee;
+import com.bt.empmgmt.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,15 +11,17 @@ import java.util.Optional;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeDAO;
 
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO){
+
+
+    public EmployeeServiceImpl(EmployeeRepository employeeDAO){
         this.employeeDAO = employeeDAO;
     }
 
     @Override
     public Employee saveEmployee(Employee employee) {
-        return this.employeeDAO.saveEmployee(employee);
+        return this.employeeDAO.save(employee);
     }
 
     @Override
