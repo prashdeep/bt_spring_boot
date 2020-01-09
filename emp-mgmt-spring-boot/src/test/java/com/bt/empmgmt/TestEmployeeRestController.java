6d5691088703950e77977c1c8810beb0b2bedf13
prashdeep <pradeep.kumar44@gmail.com>
2020-01-09 15:17:07 +0530

    committing the changes

--- emp-mgmt-spring-boot/src/test/java/com/bt/empmgmt/TestEmployeeRestController.java
+++ emp-mgmt-spring-boot/src/test/java/com/bt/empmgmt/TestEmployeeRestController.java
@@ -1,24 +1,26 @@
 package com.bt.empmgmt;
 
-import com.bt.empmgmt.controller.EmpController;
 import com.bt.empmgmt.controller.EmployeeRestController;
 import com.bt.empmgmt.model.Employee;
 import com.bt.empmgmt.service.EmployeeService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,23 +31,27 @@ public class TestEmployeeRestController {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private EmployeeService service;
+    @Autowired
+    private WebApplicationContext context;
 
-        @Before
-        public void setupMockMvc() {
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Before
+    public void setupMockMvc() {
         mockMvc = MockMvcBuilders
-            .webAppContextSetup(webContext)
-            .apply(springSecurity())
-            .build();
-        }
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
 
     @Test
     @WithMockUser(username="vinay",
-              password="welcome",
-              roles="USER")
-    public void getAllEmployeesAPI() throws Exception {
-        when(service.listAll()).thenReturn(Arrays.asList(Employee.builder().empName("Kiran").build()));
+            password="admin",
+            roles="ADMIN")
+    public void testEmployeesListAll() throws Exception {
+        when(employeeService.listAll()).thenReturn(Arrays.asList(new Employee()));
         mockMvc.perform( MockMvcRequestBuilders
                 .get("/api/v1/employees/")
                 .accept(MediaType.APPLICATION_JSON))
